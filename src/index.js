2ab08660fb02a0d1b9539c384086f8dd263bd3f3
rain84 <rainy_sky@mail.ru>
2022-03-06 15:53:08 +0300

    🔧 chore: added react-query and axios libs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { StoreProvider } from './store'
 import App from './App'
 
+const queryClient = new QueryClient()
 const rootElement = document.getElementById('root')
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
+      <QueryClientProvider client={queryClient}>
+        <StoreProvider>
+          <App />
+        </StoreProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
   rootElement
